6933cccbeb3af17710134f70e4a2fcf6622cc784
bagas <bagas.topati@gmail.com>
2020-12-17 12:26:18 +0700

    fix register

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,5 +1,7 @@
 <?php 
 
+model('UsersModel');
+
 class AuthController extends Controller
 {
 
@@ -24,26 +26,70 @@ class AuthController extends Controller
 
         $email  = $this->request('email');
         $pass   = $this->request('password');
+        $repass = $this->request('repassword');
 
         $db = Database::connect();
 
         $cari = $db->query("SELECT * FROM users WHERE user_mail='{$email}'");
         if($cari->num_rows > 0) {
 
-            Redirect::with('error', 'Email sudah digunakan')
+            Redirect::with('error', 'Email sudah digunakan!')
                 ->to(base_url('?pagename=register'));
 
+        }
+
+        if( $pass != $repass )
+        {
+
+            Redirect::with('error', 'Password tidak sama!')
+            ->to(base_url('?pagename=register'));
 
         }
+
+        $pass = md5($pass);
 
 
         $data = [
             'user_mail' => $this->request('email'),
-            'user_pass' => $this->request('password'),
+            'user_pass' => $pass,
             'id_role'   => 3
         ];
 
- 
+        $user = new UsersModel;
+        $user->insert( $data );
+
+        $query_str = "
+            SELECT 
+                users.id_user, users.id_role, users.nama_lengkap, users.user_name,
+                roles.nama_role
+            FROM users 
+            LEFT JOIN roles ON users.id_role = roles.id_role
+            WHERE user_mail='{$email}' AND user_pass='{$pass}' 
+        ";
+
+      
+
+        $cari = $db->query($query_str);
+
+
+
+        // berhasil login
+        if($cari->num_rows > 0) 
+        {
+
+            $_SESSION['user']       = $cari->fetch_array();
+            $_SESSION['is_login']   = true;
+
+            Redirect::to(base_url('?pagename=user-dashboard'));
+
+        }
+
+        // login gagal
+        Redirect::with('error', 'Email sudah digunakan')
+            ->to(base_url('?pagename=login'));
+
+
+
 
     }
 
@@ -61,16 +107,11 @@ class AuthController extends Controller
                 roles.nama_role
             FROM users 
             LEFT JOIN roles ON users.id_role = roles.id_role
-            WHERE user_name='{$username}' AND user_pass='{$password}' 
+            WHERE user_mail='{$username}' AND user_pass='{$password}' 
         ";
 
         $cari = $db->query($query_str);
 
-        //echo $query_str;
-
-        //var_dump(($cari->fetch_array()));
-
-        //die();
 
     
 
